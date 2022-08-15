7fcf872c8fbba9416fe9aedc7de25b86fb9a96ec
Wintermute766 <wintermute766@gmail.com>
2022-08-15 14:59:48 +0300

    fix3

---
+++ NewsBlog/sign/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+
+class UserView(LoginRequiredMixin, TemplateView):
+    template_name = 'sign/user.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_author'] = not self.request.user.groups.filter(name = 'authors').exists()
+        return context
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        premium_group.user_set.add(user)
+    return redirect('home')
