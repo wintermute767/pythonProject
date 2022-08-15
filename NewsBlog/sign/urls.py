ffdc6ca738dc8f25d06589457f6c3b161e3411d2
Wintermute766 <wintermute766@gmail.com>
2022-08-15 14:53:06 +0300

    fix2

--- NewsBlog/sign/urls.py
+++ NewsBlog/sign/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import UserView, LogoutView, upgrade_me
-
+from .views import UserView, upgrade_me
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('user/', UserView.as_view(template_name = 'sign/user.html'), name='user'),
     path('logout/', LogoutView.as_view(template_name='sign/logout.html'), name='logout'),
